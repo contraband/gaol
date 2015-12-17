e9284edd706b7f9f7fe1df00b2ad1a6e1e515ce8
Will Pragnell <wpragnell@gmail.com>
2015-12-17 18:33:04 +0000

    add user to stream-in and stream-out

--- commands/stream_in.go
+++ commands/stream_in.go
@@ -8,6 +8,7 @@ import (
 
 type StreamIn struct {
 	Destination string `short:"d" long:"destination" description:"destination path in the container" required:"true"`
+	User        string `short:"u" long:"user" description:"user to write the files as"`
 }
 
 func (command *StreamIn) Execute(maybeHandle []string) error {
@@ -16,6 +17,7 @@ func (command *StreamIn) Execute(maybeHandle []string) error {
 
 	streamInSpec := garden.StreamInSpec{
 		Path:      command.Destination,
+		User:      command.User,
 		TarStream: os.Stdin,
 	}
 
--- commands/stream_out.go
+++ commands/stream_out.go
@@ -9,13 +9,17 @@ import (
 
 type StreamOut struct {
 	Source string `short:"s" long:"source" description:"source path in the container" required:"true"`
+	User   string `short:"u" long:"user" description:"user to read the files as"`
 }
 
 func (command *StreamOut) Execute(maybeHandle []string) error {
 	container, err := globalClient().Lookup(handle(maybeHandle))
 	failIf(err)
 
-	streamOutSpec := garden.StreamOutSpec{Path: command.Source}
+	streamOutSpec := garden.StreamOutSpec{
+		Path: command.Source,
+		User: command.User,
+	}
 	output, err := container.StreamOut(streamOutSpec)
 	failIf(err)
 

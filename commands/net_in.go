828e1b79dabf61adfeac0cab81b8578c63f91903
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-08-06 20:41:44 -0700

    don't try to SplitHostPort when the target garden is on a unix socket

--- commands/net_in.go
+++ commands/net_in.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 type NetIn struct {
@@ -16,8 +17,13 @@ func (command *NetIn) Execute(maybeHandle []string) error {
 	hostPort, _, err := container.NetIn(0, uint32(command.Port))
 	failIf(err)
 
-	host, _, err := net.SplitHostPort(Globals.Target.Address)
-	failIf(err)
+	host := Globals.Target.Address
+	if _, err := os.Stat(host); err == nil {
+		host = "127.0.0.1"
+	} else {
+		host, _, err = net.SplitHostPort(host)
+		failIf(err)
+	}
 
 	fmt.Println(net.JoinHostPort(host, fmt.Sprintf("%d", hostPort)))
 
